bbabeedb3e5fd7da2f65cf26c182c986259b7a42
AbdulRahmanMudasser <mrhprince7@gmail.com>
2023-03-23 07:50:30 +0500

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,25 @@
+﻿class EvenOrOdd
+{
+    static void Main(string[] args)
+    {
+        // declaring variable
+        int number;
+
+        // prompting user for inpt and taking input
+        Console.Write("Enter A Number:  ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
+
+        // checking whether the entered number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("Entered Number Is Even");
+        }
+
+        else
+        {
+            Console.WriteLine("Entered Number Is Odd");
+        }
+    }
+}
